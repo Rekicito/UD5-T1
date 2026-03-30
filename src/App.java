fa51cb1c61d5e3b1b0bae7cf0d2466d6d15c3ae4
Iker <ikerhf14@gmail.com>
2026-03-30 09:52:23 +0100

    app terminado

--- src/App.java
+++ src/App.java
@@ -1,11 +1,36 @@
+import net.salesianos.guerrero;
+import net.salesianos.mago;
 import net.salesianos.personaje;
 
 public class App {
     public static void main(String[] args) {
-        personaje p1 = new personaje("Eldrin", 1);
+        personaje p1 = new personaje("Eldrin", 3);
         p1.entrenar(5);
-        p1.gastarEnergia(20);
+        p1.gastarEnergia(30);
         p1.mostrarInfo();
         personaje.mostrarTotalPersonajes();
+
+        guerrero g1 = new guerrero("Thorak", 5, "Espada");
+        g1.ataquePesado();
+        g1.descansar(10);
+        g1.mostrarInfo();
+        personaje.mostrarTotalPersonajes();
+
+        mago m1 = new mago("Lyra", 4, "Fuego");
+        m1.ataqueElemental();
+        m1.entrenar(2);
+        m1.mostrarInfo();
+        personaje.mostrarTotalPersonajes();
+
+        personaje[] reino = new personaje[3];
+
+        reino[0] = p1;
+        reino[1] = g1;
+        reino[2] = m1;
+
+        for (int i = 0; i < reino.length; i++) {
+            reino[i].mostrarInfo();
+        }
     }
+
 }
