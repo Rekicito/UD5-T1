13fd44a9a757ca95362a69fe0346cf7c093c6622
Iker <ikerhf14@gmail.com>
2026-03-30 08:53:32 +0100

    Creación de setters t getters

--- src/net/salesianos/personaje.java
+++ src/net/salesianos/personaje.java
@@ -14,4 +14,28 @@ public class personaje {
         totalPersonajes++;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
 }
