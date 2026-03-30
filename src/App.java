ed48d8be51d3b669b971785287069e463a9beb90
Iker <ikerhf14@gmail.com>
2026-03-30 09:39:50 +0100

    creación del primer personaje

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import net.salesianos.personaje;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        personaje p1 = new personaje("Eldrin", 1);
+        p1.entrenar(5);
+        p1.gastarEnergia(20);
+        p1.mostrarInfo();
+        personaje.mostrarTotalPersonajes();
     }
 }
