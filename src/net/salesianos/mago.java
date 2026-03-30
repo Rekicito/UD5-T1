26f5de3c306f2b3e1f6367340a5c4244257708af
Iker <ikerhf14@gmail.com>
2026-03-30 09:35:43 +0100

    Añadir ataque y sobreescrbir mosrarIndo

--- src/net/salesianos/mago.java
+++ src/net/salesianos/mago.java
@@ -16,4 +16,14 @@ public class mago extends personaje {
     public void setElemento(String elemento) {
         this.elemento = elemento;
     }
+
+    public void ataqueElemental() {
+        gastarEnergia(25);
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("Nivel: " + getNivel());
+        System.out.println("Elemento: " + elemento);
+    }
 }
