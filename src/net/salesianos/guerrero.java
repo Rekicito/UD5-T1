421ec70c75efa05bd758cd34716fd605b5b1b112
Iker <ikerhf14@gmail.com>
2026-03-30 09:06:20 +0100

    Empezar clase guerrero con herencia y constructor

--- src/net/salesianos/guerrero.java
+++ src/net/salesianos/guerrero.java
@@ -4,4 +4,8 @@ public class guerrero extends personaje {
 
     private String arma;
 
+    public guerrero(String nombre, int nivel, String arma) {
+        super(nombre, nivel);
+        this.arma = arma;
+    }
 }
