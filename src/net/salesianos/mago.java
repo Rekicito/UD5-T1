27ba76dc1b14c8d1c585ac87f5cf4241ff132886
Iker <ikerhf14@gmail.com>
2026-03-30 09:28:56 +0100

    Creamos la herencia de personaje y el constructor

--- src/net/salesianos/mago.java
+++ src/net/salesianos/mago.java
@@ -1,5 +1,11 @@
 package net.salesianos;
 
-public class mago {
+public class mago extends personaje {
 
+    private String elemento;
+
+    public mago(String nombre, int nivel, String elemento) {
+        super(nombre, nivel);
+        this.elemento = elemento;
+    }
 }
